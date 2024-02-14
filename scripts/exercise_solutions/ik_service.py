21994f38cba4f74b31e1067d4a2dd1aa6d01bd34
Daniel Ruan <daniel.ruan@princeton.edu>
2024-02-14 10:39:51 -0500

    Assignment 3

--- scripts/exercise_solutions/ik_service.py
+++ scripts/exercise_solutions/ik_service.py
@@ -5,6 +5,12 @@ def generate_ik_request(x, y, z):
     msg = roslibpy.ServiceRequest({
         'ik_request': {
             'group_name': 'manipulator',
+            'robot_state': {
+                'joint_state': {
+                    'name': ['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6'],
+                    'position': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                }
+            },
             'pose_stamped': {
                 'header': {
                     'frame_id': 'base_link'
@@ -33,29 +39,8 @@ client = roslibpy.Ros(host='localhost', port=9090)
 client.run()
 
 service = roslibpy.Service(client, '/compute_ik', 'moveit_msgs/GetPositionIK')
-request = roslibpy.ServiceRequest({
-    'ik_request': {
-        'group_name': 'manipulator',
-        'pose_stamped': {
-            'header': {
-                'frame_id': 'base_link'
-            },
-            'pose': {
-                'position': {
-                    'x': 0.5,
-                    'y': 0.0,
-                    'z': 0.5
-                },
-                'orientation': {
-                    'x': 0.0,
-                    'y': 0.0,
-                    'z': 0.0,
-                    'w': 1.0
-                }
-            }
-        }
-    }
-})
+msg = generate_ik_request(0.1, 0.0, 0.5)
+request = roslibpy.ServiceRequest(msg)
 result = service.call(request)
 print(result)
 
