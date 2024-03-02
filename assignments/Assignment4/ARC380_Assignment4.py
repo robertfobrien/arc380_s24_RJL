6759d9f690f716c949bc174a6be6a75b038a53a3
Robert O'Brien <ro7@princeton.edu>
2024-03-02 18:03:38 -0500

    working in the lab

--- assignments/Assignment4/ARC380_Assignment4.py
+++ assignments/Assignment4/ARC380_Assignment4.py
@@ -4,8 +4,6 @@ import compas_rrc as rrc
 
 # Define any additional imports here if needed
 
-
-
 def create_frame_from_points(point1: cg.Point, point2: cg.Point, point3: cg.Point) -> cg.Frame:
     """Create a frame from three points.
 
@@ -21,7 +19,7 @@ def create_frame_from_points(point1: cg.Point, point2: cg.Point, point3: cg.Poin
     # ================================== YOUR CODE HERE ==================================
 
     # Part 1.c.
-    frame = cg.Frame.from_points(point1, point2, point2)
+    frame = cg.Frame.from_points(point1, point2, point3)
     # ====================================================================================
     return frame
 
@@ -39,10 +37,10 @@ def transform_task_to_world_frame(ee_frame_t: cg.Frame, task_frame: cg.Frame) ->
     ee_frame_w = None
     # ================================== YOUR CODE HERE ==================================
 
-    world_frame = cg.Frame.worldXYZ()
-    T = cg.Transformation.from_frame_to_frame(task_frame, world_frame) 
-    ee_frame_w = ee_frame_t.transform(T)
-
+    world_frame = cg.Frame.worldXY()
+    T = cg.Transformation.from_frame_to_frame(world_frame, task_frame) 
+    ee_frame_t.transform(T)
+    ee_frame_w = ee_frame_t
     # ====================================================================================
     return ee_frame_w
 
@@ -56,6 +54,17 @@ def transform_task_to_world_frame(ee_frame_t: cg.Frame, task_frame: cg.Frame) ->
 
 if __name__ == '__main__':
 
+    # points in the world frame of parts of the paper
+    task_point_in_wf_1 = cg.Point(271.02,483.59, 40) #33.01
+    task_point_in_wf_3 = cg.Point(69.25, 485.12, 40) #30.30
+    task_point_in_wf_4 = cg.Point(87.88,339.13,40) #30.20
+    task_point_in_wf_2 = cg.Point(257.31,335.69,40) #32.77
+
+    task_frame = create_frame_from_points(task_point_in_wf_1, task_point_in_wf_2, task_point_in_wf_3)
+
+    print("task frame: ", task_frame)
+    #transform_task_to_world_frame(ee_frame, task_frame=task_frame)
+
     # Create Ros Client
     ros = rrc.RosClient()
     ros.run()
@@ -67,6 +76,21 @@ if __name__ == '__main__':
     # ================================== YOUR CODE HERE ==================================
 
     # Parts 1.e. and 2
+    # set the speed
+    speed = 20
+
+    # Move robot to the task frame origin
+    done = abb_rrc.send_and_wait(rrc.MoveToFrame(task_frame, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
+
+    #example move to a point [100,0,0] on task frame
+    f1 = cg.Frame([20,0,0], [1,0,0], [0,1,0]) #defines x and y axis matching the world
+    f1_p = transform_task_to_world_frame(f1, task_frame)
+    print("f1_p:", f1_p)
+    next = abb_rrc.send_and_wait(rrc.MoveToFrame(f1_p, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
+
+    # Go to home position (linear joint move)
+    #home = rrc.RobotJoints([0, 0, 0, 0, 90, 0])
+    #done = abb_rrc.send_and_wait(rrc.MoveToJoints(home, [], speed, rrc.Zone.FINE))
 
     # ====================================================================================
 
