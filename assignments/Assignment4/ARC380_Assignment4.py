ea02dfaedc2bb120c214513983f3a3f4274f96cc
Robert O'Brien <ro7@princeton.edu>
2024-03-02 20:18:18 -0500

    made some helper functions

--- assignments/Assignment4/ARC380_Assignment4.py
+++ assignments/Assignment4/ARC380_Assignment4.py
@@ -49,21 +49,38 @@ def transform_task_to_world_frame(ee_frame_t: cg.Frame, task_frame: cg.Frame) ->
 
 # Part 2
 
+# Just made this one after class.
+def goto_task_point(task_frame, x, y):
+    """Goes to the point x, y on the task frame (on the paper). x and y are in millimeters"""
+    f1 = cg.Frame([x,y,0], [1,0,0], [0,1,0])
+    f1_p = transform_task_to_world_frame(f1, task_frame)
+    print("f1_p:", f1_p)
+    next = abb_rrc.send_and_wait(rrc.MoveToFrame(f1_p, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
+
+def goto_robot_home():
+    """Go to home position (linear joint move)"""
+    home = rrc.RobotJoints([0, 0, 0, 0, 90, 0])
+    done = abb_rrc.send_and_wait(rrc.MoveToJoints(home, [], speed, rrc.Zone.FINE))
+
+def goto_task_f_origin(task_frame):
+    done = abb_rrc.send_and_wait(rrc.MoveToFrame(task_frame, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
+
+
 # ========================================================================================
 
 
 if __name__ == '__main__':
 
     # points in the world frame of parts of the paper
-    task_point_in_wf_1 = cg.Point(271.02,483.59, 40) #33.01
-    task_point_in_wf_3 = cg.Point(69.25, 485.12, 40) #30.30
-    task_point_in_wf_4 = cg.Point(87.88,339.13,40) #30.20
-    task_point_in_wf_2 = cg.Point(257.31,335.69,40) #32.77
+    # adjust z if we need to...
+    task_point_in_wf_1 = cg.Point(271.02,483.59, 40) 
+    task_point_in_wf_2 = cg.Point(257.31,335.69,40) 
+    task_point_in_wf_3 = cg.Point(69.25, 485.12, 40) 
+    task_point_in_wf_4 = cg.Point(87.88,339.13,40)
+    
 
     task_frame = create_frame_from_points(task_point_in_wf_1, task_point_in_wf_2, task_point_in_wf_3)
-
     print("task frame: ", task_frame)
-    #transform_task_to_world_frame(ee_frame, task_frame=task_frame)
 
     # Create Ros Client
     ros = rrc.RosClient()
@@ -80,17 +97,10 @@ if __name__ == '__main__':
     speed = 20
 
     # Move robot to the task frame origin
-    done = abb_rrc.send_and_wait(rrc.MoveToFrame(task_frame, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
-
-    #example move to a point [100,0,0] on task frame
-    f1 = cg.Frame([20,0,0], [1,0,0], [0,1,0]) #defines x and y axis matching the world
-    f1_p = transform_task_to_world_frame(f1, task_frame)
-    print("f1_p:", f1_p)
-    next = abb_rrc.send_and_wait(rrc.MoveToFrame(f1_p, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
-
-    # Go to home position (linear joint move)
-    #home = rrc.RobotJoints([0, 0, 0, 0, 90, 0])
-    #done = abb_rrc.send_and_wait(rrc.MoveToJoints(home, [], speed, rrc.Zone.FINE))
+    goto_task_point(task_frame, 0 , 0 )
+    
+    #moves to point 0mm, 50mm
+    goto_task_point(task_frame, 0 , 50 )
 
     # ====================================================================================
 
